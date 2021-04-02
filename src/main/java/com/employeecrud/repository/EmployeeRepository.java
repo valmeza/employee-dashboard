72a4ebd978e17f7f6c26d5bb00e31a96bd939300
Valeria Meza <valeriameza173@gmail.com>
2021-04-01 22:13:41 -0500

    add unit testing for employee service and add missing annotations.

--- src/main/java/com/employeecrud/repository/EmployeeRepository.java
+++ src/main/java/com/employeecrud/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.employeecrud.repository;
 
 import com.employeecrud.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
--- src/main/java/com/employeecrud/service/EmployeeService.java
+++ src/main/java/com/employeecrud/service/EmployeeService.java
@@ -3,10 +3,12 @@ package com.employeecrud.service;
 import com.employeecrud.exception.EmployeeNotFoundException;
 import com.employeecrud.model.Employee;
 import com.employeecrud.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class EmployeeService {
 
     private EmployeeRepository employeeRepository;
